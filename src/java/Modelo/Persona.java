6c5a86a351d10e71b8d803e01c015236d2e2c1ba
Gianmarco <Gianmarco@192.168.1.7>
2020-10-20 21:08:30 -0500

    Tablas agregadas

---
+++ src/java/Modelo/Persona.java
@@ -0,0 +1,78 @@
+
+package Modelo;
+/***Área de importación***/
+import java.sql.Date; //Clase:Tipo de dato fecha(date)
+
+/**
+ *
+ * @author Gianmarco
+ */
+public class Persona {
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private Date fechaNac;
+    private int edad;
+    private char genero;
+    private String correo;
+
+    public Persona() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+}
---
+++ src/java/Modelo/Usuario.java
@@ -0,0 +1,50 @@
+
+package Modelo;
+
+/**
+ *
+ * @author Gianmarco
+ */
+public class Usuario {
+    private int id;
+    private Persona idPersona;
+    private String usuario;
+    private String clave;
+
+    public Usuario() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Persona getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(Persona idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    
+}
