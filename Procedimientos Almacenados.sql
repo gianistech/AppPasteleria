b7448628d7848c4ae4860a9c208636554c3e1106
Gianmarco <Gianmarco@192.168.1.7>
2020-10-31 21:15:02 -0500

    Método login agregado

--- Procedimientos Almacenados.sql
+++ Procedimientos Almacenados.sql
@@ -1,3 +1,4 @@
+/*REGISTRO*/
 DELIMITER //
 CREATE PROCEDURE pa_crearUsuario(IN _nombre VARCHAR(60), IN _apellidos VARCHAR(60), IN _fechaNac Date, IN _edad INT, IN _genero VARCHAR(20), IN _correo VARCHAR(50), IN _usuario VARCHAR(40), IN _clave VARCHAR(40))
 BEGIN
@@ -9,4 +10,12 @@ BEGIN
 END//
 /*Prueba*/
 CALL pa_crearUsuario('Gian', 'Romero Castro', '1992-11-08', 27, 'Masculino', 'gianro@gmail.com', 'gian01', '1234');
+/*LOGIN*/
+DELIMITER //
+CREATE PROCEDURE pa_login(IN _usuario VARCHAR(40), IN _clave VARCHAR(40))
+BEGIN
+	SELECT * FROM tb_Usuario WHERE Usuario= _usuario AND Clave= _clave;
+END//
+/*PRUEBA*/
+CALL pa_login('gian01', '1234');
 
--- src/java/Modelo/UsuarioModel.java
+++ src/java/Modelo/UsuarioModel.java
@@ -4,6 +4,8 @@ package Modelo;
 import java.sql.Connection; //Interfaz:Implementar con el método conectar
 import Config.Conexion; //Clase:Método conectar
 import java.sql.CallableStatement; //Interfaz:Implementar con un procedimiento almacenado
+import java.sql.ResultSet; //Interfaz:Implementar objeto
+
 
 
 /**
@@ -43,6 +45,44 @@ public class UsuarioModel {
             System.out.println("UsuarioModel.insertarUsuario" + e.getMessage());
         }
     
+    }
+    
+    public Usuario login(String usuario, String clave)
+    {
+        //Conexión
+        Conexion cn = new Conexion();
+        Connection con = cn.conectar();
+        
+        String pa = "{call pa_login(?, ?)}"; //Procedimiento almacenado con parametros
+        
+        CallableStatement cs = null; //Variable referencia
+        
+        ResultSet rs = null; //Variable referencia
+        
+        Usuario objUsu = null;
+        
+        try {
+            cs = con.prepareCall(pa);//Se carga el pa en el objeto cs con la conexión BD activa
+            //Convertir los valores Java en valores SQL
+            cs.setString(1, usuario);
+            cs.setString(2, clave);
+            
+            rs = cs.executeQuery(); //Ejecutar el pa en el objeto cs contra la BD
+            
+            if(rs.next()) //Si mueve el cursor de su posición inicial hacía adelante...
+            {
+                objUsu = new Usuario(); //Instancia
+                //Obtener y establecer los valores
+                objUsu.setId(rs.getInt(1));
+                objUsu.setUsuario(rs.getString(2));
+                objUsu.setClave(rs.getString(3));
+            }
+
+        } catch (Exception e) {
+            System.out.println("UsuarioModel.Login" + e.getMessage());
+        }
+        return objUsu; //Objeto envoltura
+
     }
     
 }
